e40f7ebf148d9abfc6fa19d1f2f6cbf1b4c38372
revanthkumaar <iamrevanthkumar@gmail.com>
2021-03-01 16:47:22 +0530

    udpated app.js
    app.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,49 +7,91 @@ import Search from "./Search";
 
 const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b";
 
+
+//set initial state
+const initialState = {
+  loading:true,
+  movies: [],
+  errorMessage:null
+};
+
+const reducer = (state,action) => {
+  switch(action.type) {
+    case "SEARCH_MOVIES_REQUEST":
+      return {
+        ...state,
+        loading:true,
+        errorMessage:null
+      };
+    case "SEARCH_MOVIES_SUCCESS":
+      return {
+        ...state,
+        loading:false,
+        movies:action.payload
+      };
+    case "SEARCH_MOVIES_FAILURE":
+      return {
+        ...state,
+        loading:false,
+        errorMessage:action.error
+      };
+    default:
+      return state;
+  }
+}
+
 const App = () => {
-  //variables/parameters intialization/declaration
-  const [loading,setLoading] = useState(true);
-  const [movies,setMovies] = useState([]);
-  const [errorMessage,setErrorMessage] = useState(null);
+  const [state,dispatch] = useReducer(reducer,initialState);
 
   useEffect(() => {
     fetch(MOVIE_API_URL)
     .then(response => response.json())
     .then(jsonResponse => {
-      setMovies(json.Response.Search);
-      setLoading(false);
-    })
-  },[])
+      dipatch({
+          type: "SEARCH_MOVIES_SUCCESS",
+          payload: jsonResponse.Search
+      });
+    });
+  },[]);
 
   const search = searchValue => {
-    //complete search functionality
-    setLoading(true);
-    setErrorMessage(null);
-    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
-    .then(response=> response.json())
-    .then(jsonResponse => {
-      if(jsonResponse.Reponse === "True"){
-        //when there is response and some information
-        setMovies(jsonReponse.Search);
-        setLoading(false);
-      }
-      else{
-        //when there is no response
-        setErrorMessage(jsonResponse.Error);
-        setLoading(false);
-      }
+    //to show the loader by changing the initial state
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST",
+    });
 
-    })
+  //make the API call
+  const search = searchValue => {
+    dispatch({
+      type: "SEARCH_MOVIES_REQUEST"
+    });
+
+      fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === "True") {
+          dispatch({
+              type: "SEARCH_MOVIES_SUCCESS",
+              payload: jsonResponse.Search
+          });
+        } else {
+          dispatch({
+              type: "SEARCH_MOVIES_FAILURE",
+              error: jsonResponse.Error
+          });
+        }
+      });
+  };
+
+
+  const movies,errorMessage, loading = state;
+
+
+  return(
+
+  );
+
   }
 
 
-
-
-
-
-
-
-  
-
- }
+}
